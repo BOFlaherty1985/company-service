253b874f72d3546736859d74557a682c4d016124
Benjamin OFlaherty <b_oflaherty@hotmail.com>
2022-01-25 16:23:37 +0000

    Add Spring Security and update bean configurations for JWT security

--- src/main/java/com/investment/companyservice/CompanyServiceApplication.java
+++ src/main/java/com/investment/companyservice/CompanyServiceApplication.java
@@ -1,5 +1,8 @@
 package com.investment.companyservice;
 
+import com.com.investment.websecuritylibrary.filter.JwtRequestFilter;
+import com.com.investment.websecuritylibrary.jwt.JwtUtility;
+import com.com.investment.websecuritylibrary.service.CustomUserDetailService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
@@ -11,6 +14,21 @@ public class CompanyServiceApplication {
     @Bean
     public RestTemplate restTemplate() {
         return new RestTemplate();
+    }
+
+    @Bean
+    public JwtUtility jwtUtility() {
+        return new JwtUtility();
+    }
+
+    @Bean
+    public JwtRequestFilter jwtRequestFilter() {
+        return new JwtRequestFilter();
+    }
+
+    @Bean
+    public CustomUserDetailService customUserDetailService() {
+        return new CustomUserDetailService();
     }
 
     public static void main(String[] args) {
